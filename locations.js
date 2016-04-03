5085160e135cb96dfbbdb9b6fe4654f666d54931
Brian <blanketyblankman@gmail.com>
2016-04-03 18:17:41 -0500

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,63 @@
+var bshields = bshields || {};
+bshields.locations = {
+    adventure:          { left: 814, top: 315 },
+    scenario:           { left: 980, top: 315 },
+    blessingDiscard:    { left: 710, top: 617 },
+    blessingDeck:       { left: 930, top: 617 },
+    
+    locations: [
+        { deck: { left: 1292, top: 269 },  card: { left: 1456, top: 269 } },
+        { deck: { left: 1292, top: 777 },  card: { left: 1456, top: 777 } },
+        { deck: { left: 1292, top: 1275 }, card: { left: 1456, top: 1275 } },
+        { deck: { left: 921,  top: 1275 }, card: { left: 1085, top: 1275 } },
+        { deck: { left: 547,  top: 1275 }, card: { left: 711,  top: 1275 } },
+        { deck: { left: 165,  top: 1275 }, card: { left: 329,  top: 1275 } },
+        { deck: { left: 165,  top: 777 },  card: { left: 329,  top: 777 } },
+        { deck: { left: 165,  top: 269 },  card: { left: 329,  top: 269 } }
+    ],
+    
+    players: [
+        {
+            character: { left: 1799, top: 175 },
+            deck: { left: 2002, top: 207 },
+            discard: { left: 2184, top: 207 },
+            bury: { left: 2388, top: 207, rotation: 90 },
+            hand: { left: 1733, top: 451 }
+        },
+        {
+            character: { left: 1799, top: 669 },
+            deck: { left: 2002, top: 704 },
+            discard: { left: 2184, top: 704 },
+            bury: { left: 2388, top: 704, rotation: 90 },
+            hand: { left: 1733, top: 946 }
+        },
+        {
+            character: { left: 1799, top: 1172 },
+            deck: { left: 2002, top: 1204 },
+            discard: { left: 2184, top: 1204 },
+            bury: { left: 2388, top: 1204, rotation: 90 },
+            hand: { left: 1733, top: 1449 }
+        },
+        {
+            character: { left: 2918, top: 175 },
+            deck: { left: 3127, top: 207 },
+            discard: { left: 3317, top: 207 },
+            bury: { left: 3513, top: 207, rotation: 90 },
+            hand: { left: 2852, top: 451 }
+        },
+        {
+            character: { left: 2918, top: 669 },
+            deck: { left: 3127, top: 704 },
+            discard: { left: 3317, top: 704 },
+            bury: { left: 3513, top: 704, rotation: 90 },
+            hand: { left: 2852, top: 946 }
+        },
+        {
+            character: { left: 2918, top: 1172 },
+            deck: { left: 3127, top: 1204 },
+            discard: { left: 3317, top: 1204 },
+            bury: { left: 3513, top: 1204, rotation: 90 },
+            hand: { left: 2852, top: 1449 }
+        }
+    ]
+};
